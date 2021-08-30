d194e28b84e65d9f260fd460cd3aec23c621c2f2
AmagiDDmxh <amagiddmxh@qq.com>
2021-08-31 00:31:56 +0800

    Update language constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,33 @@
-export const DEEPL_API_URL = "https://api.deepl.com/v2";
+export const DEEPL_API_URL = "https://api-free.deepl.com/v2";
 
+// TODO: Add target languages specified here: https://www.deepl.com/docs-api/translating-text/#Request%20Parameters
 export const LANGUAGES = {
-  englishAmerican: "en-US",
-  englishBritish: "en-GB",
-  germen: "de",
+  bulgarian: "bg",
+  czech: "cs",
+  danish: "da",
+  german: "de",
+  greek: "el",
+  english: "en",
   spanish: "es",
-  frensh: "fr",
+  estonian: "et",
+  finnish: "fi",
+  french: "fr",
+  hungarian: "hu",
   italian: "it",
   japanese: "ja",
+  lithuanian: "lt",
+  latvian: "lv",
   dutch: "nl",
   polish: "pl",
   portuguese: "pt",
+  romanian: "ro",
   russian: "ru",
-  chinese: "zh",
-};
+  slovak: "sk",
+  slovenian: "sl",
+  swedish: "sv",
+  chinese: "zh"
+} as const
+
+type ValuesOf<ObjectType, ValueType extends keyof ObjectType = keyof ObjectType> = ObjectType[ValueType];
+export type LangVal = ValuesOf<typeof LANGUAGES>
+export type LangKey = keyof typeof LANGUAGES
