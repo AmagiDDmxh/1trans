70ad4878eb49fb5d33d5093da883e20d2de3d548
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-01 01:37:17 +0800

    Add `createOutputPath` util to make default output

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { LangKey, LANGUAGES } from "./constants";
+
 export const REGEX_CHINESE = /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u;
 
 export const isCn = (str: string) => {
@@ -17,3 +19,21 @@ export const mapObject = <T, O>(obj: any, fn: (v: T) => O) => {
     {}
   );
 };
+
+interface CreateOutputPathArgs {
+  to: string
+  from?: string
+  inputFile: string
+}
+
+export const createOutputPath = ({ inputFile, from, to }: CreateOutputPathArgs) => {
+  if (from) {
+    return `${inputFile.replace('.json', '').replace(`-${from}`, '')}-${to}.json`
+  }
+  const matches = inputFile.match( /[\.-]+(.*).json/)
+
+  if (matches) {
+    const [, matchedFrom] = matches
+    return `${inputFile.replace('.json', '').replace(matchedFrom, to)}.json`
+  }
+}
